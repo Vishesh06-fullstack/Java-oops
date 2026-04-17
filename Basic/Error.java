4daa4e0197acb36520c3b14169604fbc2cd3e0fb
vishesh singh <visheshpratapsingh12@gmail.comgit config --global user.name>
2026-04-17 10:46:07 +0530

    error include

---
+++ Basic/Error.java
@@ -0,0 +1,17 @@
+class Bike{
+    private String name;
+    private String type;
+
+    void start(){
+        System.out.println("Starting the " + name + " bike of type " + type);
+    }
+}
+
+
+public class Error {
+    public static void main(String[] args) {
+        Bike bike1 = new Bike();
+        bike1.name = "Yamaha"; // Error: name has private access in Bike
+        bike1.type = "sport"; // Error : type has private access in bike    }
+    
+}
